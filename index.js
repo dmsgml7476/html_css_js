6c56ce3cbed04d2a113ce7d6c511345d5ec74661
dmsgml7476 <dmsgml7476@gmail.com>
2025-02-17 15:48:23 +0900

    rsp init

---
+++ index.js
@@ -0,0 +1,7 @@
+const imgs = [
+  "https://cdn-icons-png.flaticon.com/128/9534/9534501.png",
+  "https://cdn-icons-png.flaticon.com/128/13992/13992554.png",
+  "https://cdn-icons-png.flaticon.com/128/12355/12355903.png",
+];
+
+console.log(imgs);
