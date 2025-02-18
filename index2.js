476d5daea4ccbb8205833bda55a87cfea1623092
dmsgml7476 <dmsgml7476@gmail.com>
2025-02-18 17:01:32 +0900

    dice end

---
+++ index2.js
@@ -0,0 +1,8 @@
+let dice = [
+  "https://cdn-icons-png.flaticon.com/128/10826/10826863.png",
+  "https://cdn-icons-png.flaticon.com/128/10826/10826864.png",
+  "https://cdn-icons-png.flaticon.com/128/10826/10826865.png",
+  "https://cdn-icons-png.flaticon.com/128/10826/10826866.png",
+  "https://cdn-icons-png.flaticon.com/128/1626/1626822.png",
+  "https://cdn-icons-png.flaticon.com/128/10826/10826868.png",
+];
