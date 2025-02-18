2724fb57aa58258e91d6b32f204eed756621cfb2
dmsgml7476 <dmsgml7476@gmail.com>
2025-02-18 17:28:48 +0900

    init

--- index2.js
+++
@@ -1,8 +0,0 @@
-let dice = [
-  "https://cdn-icons-png.flaticon.com/128/10826/10826863.png",
-  "https://cdn-icons-png.flaticon.com/128/10826/10826864.png",
-  "https://cdn-icons-png.flaticon.com/128/10826/10826865.png",
-  "https://cdn-icons-png.flaticon.com/128/10826/10826866.png",
-  "https://cdn-icons-png.flaticon.com/128/1626/1626822.png",
-  "https://cdn-icons-png.flaticon.com/128/10826/10826868.png",
-];
